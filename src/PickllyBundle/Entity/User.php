01cfff00066f0d1f6c2dbd6f83599c13bc264d28
louri45 <l.richard45@gmail.com>
2016-12-14 15:40:19 +0100

    FOSUser

---
+++ src/PickllyBundle/Entity/User.php
@@ -0,0 +1,21 @@
+<?php
+    
+
+namespace PickllyBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
