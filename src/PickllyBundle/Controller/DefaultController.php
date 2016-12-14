399c36e7157335d917cd536e69d2f9d375404139
Gy0m <landreaug@gmail.com>
2016-12-14 15:22:56 +0100

    generate bundle

---
+++ src/PickllyBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PickllyBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('PickllyBundle:Default:index.html.twig');
+    }
+}
